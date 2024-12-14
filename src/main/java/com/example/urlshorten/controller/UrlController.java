c15acd31513cef5127c735595eeaef23aa61f6d0
AhmedSamehh <a7medse7a10@gmail.com>
2024-12-14 03:03:34 +0200

    creating get original url and added some enhancements

--- src/main/java/com/example/urlshorten/controller/UrlController.java
+++ src/main/java/com/example/urlshorten/controller/UrlController.java
@@ -3,11 +3,9 @@ package com.example.urlshorten.controller;
 import com.example.urlshorten.model.Url;
 import com.example.urlshorten.service.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -17,9 +15,20 @@ public class UrlController {
     @Autowired
     private UrlService urlService;
 
+    @Value("${app.base-url}")
+    private String baseUrl;
+
     @PostMapping("/shorten")
     public ResponseEntity<Url> shortenUrl(@RequestBody Map<String,String> urlMap) {
         Url createdUrl = urlService.createShortUrl(urlMap.get("originalUrl"));
+        String fullShortUrl = baseUrl + createdUrl.getShortUrl();
+        createdUrl.setShortUrl(fullShortUrl);
         return ResponseEntity.ok(createdUrl);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<String> getOriginalUrl(@PathVariable String shortUrl) {
+        String originalUrl = urlService.getOriginalUrl(shortUrl);
+        return ResponseEntity.ok(originalUrl);
     }
 }
--- src/main/java/com/example/urlshorten/service/UrlService.java
+++ src/main/java/com/example/urlshorten/service/UrlService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UrlService {
@@ -21,18 +22,36 @@ public class UrlService {
             throw new IllegalArgumentException("Invalid URL provided");
         }
 
+        Optional<Url> existingUrl = urlRepository.findByOriginalUrl(urlToShorten);
+
+        if (existingUrl.isPresent()) {
+            return existingUrl.get(); // Return the existing entry
+        }
+
         Url url = new Url();
         url.setOriginalUrl(urlToShorten);
-        url.setShortUrl(BASE_SHORT_URL + generateUniqueShortUrl());
+        url.setShortUrl(generateUniqueShortUrlId());
         url.setCreatedAt(LocalDateTime.now());
         return urlRepository.save(url);
     }
 
-    private String generateUniqueShortUrl() {
+    private String generateUniqueShortUrlId() {
         String shortUrlId;
         do {
             shortUrlId = RandomStringUtils.randomAlphanumeric(8);
-        } while (urlRepository.findByShortUrl(BASE_SHORT_URL + shortUrlId).isPresent());
+        } while (urlRepository.findByShortUrl(shortUrlId).isPresent());
         return shortUrlId;
+    }
+
+    public String getOriginalUrl(String shortUrlId) {
+        if(shortUrlId.isEmpty())
+            throw new RuntimeException("Short URL identifier not found cannot be empty");
+        Url url = urlRepository.findByShortUrl(shortUrlId)
+                .orElseThrow(() -> new RuntimeException("Short URL not found"));
+
+        url.setAccessCount(url.getAccessCount() + 1);
+        urlRepository.save(url);
+
+        return url.getOriginalUrl();
     }
 }
