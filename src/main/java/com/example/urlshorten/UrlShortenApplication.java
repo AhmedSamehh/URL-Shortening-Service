f65710aa29b3988b9bb39de0cc3822179426ae97
asameh <a7medse7a10@gmail.com>
2025-02-20 10:34:04 +0200

    renaming project

--- src/main/java/com/example/urlshorten/UrlShortenApplication.java
+++ src/main/java/com/example/urlshorten/UrlShortenApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrlshortenApplication {
+public class UrlShortenApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrlshortenApplication.class, args);
+		SpringApplication.run(UrlShortenApplication.class, args);
 	}
 
 }
